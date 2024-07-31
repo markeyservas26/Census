b65b27a3b48af98647ea2f099de5e884b672d1af
markeyservas26 <makoybatasinin08@gmail.com>
2024-07-31 06:54:29 -0700

    Update form.php

--- staffbantayan/form.php
+++ staffbantayan/form.php
@@ -40,9 +40,7 @@
             <div class="mb-3">
                 <label class="form-label">Municipality:</label>
                 <select class="form-select" name="municipality">
-                    <option value="madridejos">Madridejos</option>
                     <option value="bantayan">Bantayan</option>
-                    <option value="santafe">Santa Fe</option>
                 </select>
             </div>
             <div class="mb-3">
@@ -314,14 +312,6 @@ document.addEventListener('DOMContentLoaded', function() {
             'Atop-Atop', 'Bigad', 'Bantigue', 'Baod', 'Binaobao', 'Botigues', 'Doong', 'Guiwanon', 'Hilotongan',
             'Kabac', 'Kabangbang', 'Kampingganon', 'Kangkaibe', 'Lipayran', 'Luyongbaybay', 'Mojon',
             'Oboob', 'Patao', 'Putian', 'Sillion', 'Suba', 'Sulangan', 'Sungko', 'Tamiao', 'Ticad'
-        ],
-        madridejos: [
-            'Poblacion', 'Mancilang', 'Malbago', 'Kaongkod', 'San Agustin', 'Kangwayan', 'Pili', 'Kodia',
-            'Tabagak', 'Bunakan', 'Maalat', 'Tugas', 'Tarong', 'Talangnan'
-        ],
-        santafe: [
-            'Balidbid', 'Hagdan', 'Hilantagaan', 'Kinatarkan', 'Langub', 'Marikaban', 'Okoy', 'Poblacion',
-            'Pooc', 'Talisay'
         ]
     };
 
