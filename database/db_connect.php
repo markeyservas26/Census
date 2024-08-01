e7231222f47eb80fd0f0811c6ba52ac9e5a23e37
markeyservas26 <makoybatasinin08@gmail.com>
2024-08-01 03:06:02 -0700

    Update db_connect.php

--- database/db_connect.php
+++ database/db_connect.php
@@ -3,7 +3,7 @@
 $servername = "127.0.0.1";
 $username = "u510162695_bantayanisland"; // Change to your MySQL username
 $password = "1Bantayan"; // Change to your MySQL password
-$dbname = "u510162695_bantayanisland"; // Database name
+$dbname = "census"; // Database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
