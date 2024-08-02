d85cd16819b795e5aeb18be1250e24851e75f665
Mark John <markjohnbatasinin707@gmail.com>
2024-08-01 18:06:35 -0700

    new

--- database/db_connect.php
+++ database/db_connect.php
@@ -3,7 +3,7 @@
 $servername = "127.0.0.1";
 $username = "u510162695_bantayanisland"; // Change to your MySQL username
 $password = "1Bantayan"; // Change to your MySQL password
-$dbname = "census"; // Database name
+$dbname = "u510162695_bantayanisland"; // Database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
