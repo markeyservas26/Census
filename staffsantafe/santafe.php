4b2a747247b01ff231562d0fe5f4a3b9b91a73b2
Mark John <markjohnbatasinin707@gmail.com>
2024-08-03 03:18:36 -0700

    newse

--- staffsantafe/santafe.php
+++ staffsantafe/santafe.php
@@ -418,7 +418,7 @@ body {
     <div class="modal-content" >
       <div class="modal-header">
         <h5 class="modal-title" id="viewModalLabel" >Person Details</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button" class="btn btn-info" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body" id="print-section">
         <div id="modal-content">
