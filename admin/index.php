405f5a58728b112a467396ee9c9f77482e4092d8
Mark John <markjohnbatasinin707@gmail.com>
2024-08-04 01:14:59 -0700

    asdasd

--- admin/index.php
+++ admin/index.php
@@ -1,15 +1,4 @@
 <?php
-
-include '../session.php';
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache"); 
-// Check if the user is logged in
-if (!isset($_SESSION['userid'])) {
-    header("Location:login.php");
-    exit;
-}
-
 include 'header.php';
 include '../database/db_connect.php';
 
