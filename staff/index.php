4ed5082196fe5caa2160bb435f94deaab3868f73
Mark John <markjohnbatasinin707@gmail.com>
2024-08-03 22:42:05 -0700

    sd

--- staff/index.php
+++ staff/index.php
@@ -1,11 +1,4 @@
-<?php
-session_start();
 
-if (!isset($_SESSION['user_id'])) {
-    header("Location:login.php"); // Redirect to login page if not logged in
-    exit();
-}
-?>
 <?php include 'header.php'; ?>
 
 
