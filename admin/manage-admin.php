d60b6a6196ab1913ea8460a1ad9ab3abd6755d4c
Mark John <markjohnbatasinin707@gmail.com>
2024-11-09 12:53:29 -0800

    awd

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -762,3 +762,7 @@ function printDetails() {
 }
 </script>
 
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+
