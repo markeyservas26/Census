8453abded81f9b12ef97b050ff97a68b8628d70e
Mark John <markjohnbatasinin707@gmail.com>
2024-11-14 15:15:54 -0800

    new

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -812,5 +812,4 @@ function printDetails() {
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
 
-<?php include 'footer.php'; ?>
 
