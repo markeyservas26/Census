051e7a259ba5aa1843e99f59c199f8f973db329d
Mark John <markjohnbatasinin707@gmail.com>
2024-11-14 16:10:10 -0800

    new

--- admin/manage-staff.php
+++ admin/manage-staff.php
@@ -242,7 +242,7 @@ $result = $stmt->get_result();
                 <p><strong>Municipality:</strong> <span id="modal-municipality"></span></p>
                 <div>
                     <label for="modal-password" class="form-label"><strong>Password (staff only):</strong></label>
-                    <input type="text" id="modal-password" class="form-control" value="staff" readonly />
+                    <input type="text" id="modal-password" class="form-control" value="staff">
                 </div>
             </div>
             <div class="modal-footer">
