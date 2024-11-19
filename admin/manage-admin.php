4b7df43abb1d4cb7c828af896ecded810e359a63
Mark John <markjohnbatasinin707@gmail.com>
2024-11-18 23:49:28 -0800

    new

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -376,9 +376,7 @@ $result = $stmt->get_result();
   </div>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
 function validatePassword() {
     const password = document.getElementById('modal-password').value;
@@ -860,6 +858,8 @@ function printDetails() {
 </script>
 
 <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 <script src="assets/js/main.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
