ae5188865b30c63146b05a2d889a46a3ebeb2ac8
Mark John <markjohnbatasinin707@gmail.com>
2024-11-18 23:39:53 -0800

    new

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -859,9 +859,9 @@ function printDetails() {
 }
 </script>
 
-<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 <script src="assets/js/main.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
