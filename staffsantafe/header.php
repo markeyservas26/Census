eea0b512a4744869bf45d246fe8cb75a7fbe8694
Mark John <markjohnbatasinin707@gmail.com>
2024-11-26 20:44:28 -0800

    new

--- staffsantafe/header.php
+++ staffsantafe/header.php
@@ -8,7 +8,7 @@ session_start();
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Dashboard - Bantayan Island Census</title>
+  <title>Staff Home - Bantayan Island Census</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
