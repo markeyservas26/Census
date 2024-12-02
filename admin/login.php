c47c0a088af1f5804f6b79e5c1980ebc2c3586c1
Mark John <markjohnbatasinin707@gmail.com>
2024-12-02 06:02:11 -0800

    new

--- admin/login.php
+++ admin/login.php
@@ -194,19 +194,19 @@ window.addEventListener('load', function() {
 
 
 
-  document.addEventListener('DOMContentLoaded', function() {
-    const togglePassword = document.querySelector('#togglePassword');
-    const passwordField = document.querySelector('#yourPassword');
+document.addEventListener('DOMContentLoaded', function() {
+      const togglePassword = document.querySelector('#togglePassword');
+      const passwordField = document.querySelector('#yourPassword');
 
-    togglePassword.addEventListener('click', function() {
-      // Toggle the type attribute
-      const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
-      passwordField.setAttribute('type', type);
+      togglePassword.addEventListener('click', function() {
+        // Toggle the type attribute
+        const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordField.setAttribute('type', type);
 
-      // Toggle the eye icon
-      this.querySelector('i').classList.toggle('fa-eye-slash');
+        // Toggle the eye icon
+        this.querySelector('i').classList.toggle('fa-eye-slash');
+      });
     });
-  });
 
   // Validation for the login form
   document.getElementById('loginForm').addEventListener('submit', function(event) {
