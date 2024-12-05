a7f91fd4e519a99df8bd0b65718eec17d848c356
Mark John <markjohnbatasinin707@gmail.com>
2024-12-04 18:11:00 -0800

    new

--- loading.php
+++ loading.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../assets/img/travelogo.png" rel="icon">
-    <title>Human Verification</title>
+    <title>Just a Moment...</title>
     <!-- Tailwind CSS CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
     <!-- Google reCAPTCHA v3 -->
