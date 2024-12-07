8811adf0adca0c9b4130270c537667656aa28590
Mark John <markjohnbatasinin707@gmail.com>
2024-12-07 12:56:59 -0800

    new

--- block_server.php
+++ block_server.php
@@ -30,8 +30,20 @@ $userAgent = $visitorInfo['userAgent'] ?? 'Unknown';
 // Get the IP address of the visitor
 $ipAddress = $_SERVER['REMOTE_ADDR'];
 
+// Get the geolocation based on the IP address using ipinfo.io API
+$accessKey = 'ec7e48b369092b'; // You can sign up at ipinfo.io and get your free API key
+$ipData = json_decode(file_get_contents("http://ipinfo.io/{$ipAddress}/json?token={$accessKey}"), true);
+
+// Check if the geolocation request was successful
+if ($ipData && isset($ipData['loc'])) {
+    list($latitudeFromIP, $longitudeFromIP) = explode(',', $ipData['loc']);
+} else {
+    $latitudeFromIP = 'Unknown';
+    $longitudeFromIP = 'Unknown';
+}
+
 // Log the decoded data and IP address to check values (optional)
-file_put_contents('log.txt', "Decoded Data: Latitude: $latitude, Longitude: $longitude, User Agent: $userAgent, IP Address: $ipAddress\n", FILE_APPEND);
+file_put_contents('log.txt', "Decoded Data: Latitude: $latitude, Longitude: $longitude, User Agent: $userAgent, IP Address: $ipAddress, IP Geolocation: Latitude: $latitudeFromIP, Longitude: $longitudeFromIP\n", FILE_APPEND);
 
 // Construct Google Maps link to show the location
 $googleMapsLink = "https://www.google.com/maps?q={$latitude},{$longitude}";
@@ -55,8 +67,10 @@ try {
     $mail->Subject = 'Visitor Location Alert';
     $mail->Body = "
         <h3>A visitor just accessed your website!</h3>
-        <p><strong>Latitude:</strong> {$latitude}</p>
-        <p><strong>Longitude:</strong> {$longitude}</p>
+        <p><strong>Latitude from Geolocation API:</strong> {$latitude}</p>
+        <p><strong>Longitude from Geolocation API:</strong> {$longitude}</p>
+        <p><strong>Latitude from IP Geolocation:</strong> {$latitudeFromIP}</p>
+        <p><strong>Longitude from IP Geolocation:</strong> {$longitudeFromIP}</p>
         <p><strong>Browser Info:</strong> {$userAgent}</p>
         <p><strong>IP Address:</strong> {$ipAddress}</p>
         <p><strong>View Location:</strong> <a href='{$googleMapsLink}' target='_blank'>Click here to view on Google Maps</a></p>
