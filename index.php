f8e4021f093b82563844591c08b32f6c53962444
Mark John <markjohnbatasinin707@gmail.com>
2024-12-08 02:31:59 -0800

    new

--- index.php
+++ index.php
@@ -728,7 +728,6 @@ include 'block_server.php';
                             <p class="text-justify" id="census-paragraph" style="margin-top: 30px;">Help us shape the future of our community by participating in the census. Your input is crucial for better planning and services. Thank you for being involved!</p>
                         </div>
                     </div>
-                    <script src="//code.tidio.co/xjhnpy1s8nfzl3muds8u7acrmf1p3jkc.js" async></script>
                 </div>
             </div>
         </div>
