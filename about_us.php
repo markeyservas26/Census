5326c1f5c6def6052a7bde79e43a8aa9e00e9830
Mark John <markjohnbatasinin707@gmail.com>
2024-12-08 05:11:53 -0800

    new

--- about_us.php
+++ about_us.php
@@ -914,6 +914,10 @@
             </div>
         </div>
     </div>
+    <!-- Bootstrap JS (with Popper.js) -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+
 
 <script>
         document.addEventListener("DOMContentLoaded", function() {
