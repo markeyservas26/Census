27c573b555b6d63aa8ba06ba514b8467c8ce9335
Mark John <markjohnbatasinin707@gmail.com>
2024-12-11 18:53:01 -0800

    new

--- staffbantayan/view_household.php
+++ staffbantayan/view_household.php
@@ -1305,13 +1305,7 @@ function safe_array_value($array, $key, $default = '') {
 
 </div>
                
-                  
-<div class="text-center mt-4">
-    <form id="myForm" onsubmit="return false;"> <!-- Prevent default form submission -->
-        <!-- Your form fields here -->
-        <button type="button" onclick="goToPrintForm()">View Print Form</button>
-    </form>
-</div>
+                
             </form>
         </div>
     </main>
