91fe8de58e6644a1d27f307f35a72a908e965e8d
Mark John <markjohnbatasinin707@gmail.com>
2024-12-12 16:09:19 -0800

    sddd

--- staff/import_data.php
+++ staff/import_data.php
@@ -1,6 +1,6 @@
 <?php
 // Database connection details
-$servername = "ns1.dns-parking.com";
+$servername = "ns2.dns-parking.com";
 $username = "u510162695_bantayanisland"; // Change to your MySQL username
 $password = "1Bantayan"; // Change to your MySQL password
 $dbname = "u510162695_bantayanisland"; // Database name
