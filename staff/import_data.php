151eaaa2422327ea28ce6ea4425ea1f2705b332a
Mark John <markjohnbatasinin707@gmail.com>
2024-12-12 16:06:04 -0800

    dddd

--- staff/import_data.php
+++ staff/import_data.php
@@ -1,6 +1,6 @@
 <?php
 // Database connection details
-$servername = "77.37.34.108";
+$servername = "127.0.0.1";
 $username = "u510162695_bantayanisland"; // Change to your MySQL username
 $password = "1Bantayan"; // Change to your MySQL password
 $dbname = "u510162695_bantayanisland"; // Database name
