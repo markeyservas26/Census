565b5eb0339a18ef4f775bea6253791ac27ec683
Mark John <markjohnbatasinin707@gmail.com>
2024-12-14 11:18:30 -0800

    new

--- staff/import_data.php
+++ staff/import_data.php
@@ -1,7 +1,7 @@
 <?php
 
 // Database connection details
-$servername = "bantayanislandcensus.com";
+$servername = "server1376";
 $username = "u510162695_bantayanisland"; // Change to your MySQL username
 $password = "1Bantayan"; // Change to your MySQL password
 $dbname = "u510162695_bantayanisland"; // Database name
